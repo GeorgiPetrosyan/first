7b42e268ae7385c6fec8c22427864dd7055150b0
GeorgiPetrosyan <gpetrosyan99@gmail.com>
2024-06-29 23:07:41 +0400

    Delete web directory

--- web/script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('submitButton').addEventListener('click', function() {
-    const inputField = document.getElementById('inputField');
-    const resultText = document.getElementById('resultText');
-    resultText.textContent = `Вы ввели: ${inputField.value}`;
-});
