1fa93b783f4fb6e9e4abbecba2c41ec72d623526
GeorgiPetrosyan <gpetrosyan99@gmail.com>
2024-06-30 00:50:16 +0400

    Add files via upload

---
+++ first.py
@@ -0,0 +1,29 @@
+import flet as ft
+def main(page: ft.Page):
+    page.title = "Простое приложение на Flet"
+
+    header = ft.Text("Добро пожаловать в мое простое приложение!", style="headlineLarge")
+    input_field = ft.TextField(label="Введите сообщение")
+    result_text = ft.Text()
+
+
+    def on_button_click():
+        result_text.value = f"Вы ввели: {input_field.value}"
+        print(f"New user with text {input_field.value}")
+        page.update()
+
+
+    submit_button = ft.ElevatedButton(text="Отправить", on_click=on_button_click)
+
+
+    page.add(
+        header,
+        input_field,
+        submit_button,
+        result_text
+    )
+
+
+if __name__ == "__main__":
+    ft.app(main)
+# ft.app(target=main, view=None, port=8888)
